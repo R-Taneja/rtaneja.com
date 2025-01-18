5f9200b94d34d166b76f906fbfba706442f697f7
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2025-01-18 15:20:47 -0500

    Add BuildList project

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -7,7 +7,8 @@ import { useNavigate } from "react-router-dom";
 import blokkerLogo from '../img/blokker.webp';
 import rapidTestLogo from '../img/rapidtest.webp';
 import wordFreqLogo from '../img/wordfreq.webp';
-import driveTestLogo from '../img/driveTestLogo.webp';
+// import driveTestLogo from '../img/driveTestLogo.webp';
+import buildListLogo from '../img/buildListLogo.png';
 import notedLogo from '../img/notedLogo.webp';
 import auxifyLogo from '../img/auxifyLogo.webp';
 import dropInCSS from '../img/dropInCSS.webp';
@@ -24,6 +25,7 @@ function Projects() {
           <FontAwesomeIcon className="backButton" size="xl" icon={faArrowLeft} onClick={() => {navigate("/")}} />
           <h2 className='projectsHeader'>Featured Projects</h2>
           <div className="projects">
+            <Project title='BuildList' description='Discover your next profitable idea with weekly insights on the app market' language='</>  Next.js' link='https://www.buildlist.app/' logo={buildListLogo} />
             <Project title='Noted' description='Jot down thoughts and ideas instantly with this convenient note-taking app for the macOS menu bar' language='</>  Swift • 1.5K+ downloads • 4.6 &#9733;' link='https://apps.apple.com/us/app/noted/id1541403993' logo={notedLogo} />
             <Project title='Auxify' description='Create interactive listening experiences by making your Spotify queue collaborative' language='</>  React • 1K+ users' link='https://www.auxify.io' logo={auxifyLogo} />
             <Project title='Permagen' description='Generate permalinks instantly for any file with this CLI' language='</>  JS • 350+ downloads' link='https://www.npmjs.com/package/permagen' logo={permagenLogo} />
@@ -31,7 +33,7 @@ function Projects() {
             <Project title='Rapid Test Collector' description='Host events safely by collecting COVID-19 test results from attendees' language='</>  React' link='https://rapid-test-collector.vercel.app' logo={rapidTestLogo} />
             <Project title='Word Frequency Tool' description='Write with clarity by cutting down on repeated words' language='</>  HTML, CSS, & JS' link='https://wordfrequency.vercel.app' logo={wordFreqLogo} />
             <Project title='Blokker' description='Stay focused while using Instagram by blocking only the most distracting features' language='</>  Chrome Extension' link='https://github.com/R-Taneja/Blokker' logo={blokkerLogo} />
-            <Project title='DriveTest Bot' description="Get your Ontario driver's license faster with this bot that checks for available road test dates in seconds" language='</>  Python' link='https://github.com/R-Taneja/DriveTest-Date-Checker' logo={driveTestLogo} />
+            {/* <Project title='DriveTest Bot' description="Get your Ontario driver's license faster with this bot that checks for available road test dates in seconds" language='</>  Python' link='https://github.com/R-Taneja/DriveTest-Date-Checker' logo={driveTestLogo} /> */}
           </div>
           <button className="githubButton" onClick={() => {window.open("https://github.com/R-Taneja")}}>More on GitHub &nbsp;<FontAwesomeIcon icon={faUpRightFromSquare} /></button>
       </div>
